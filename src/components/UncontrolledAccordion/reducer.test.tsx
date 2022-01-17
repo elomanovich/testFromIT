e6aa7b403bcd7b9e7d3fec74c2dbc12c84149507
jiemoh <jiemoh@list.ru>
2022-01-17 23:28:49 +0300

    added fake test

--- src/components/UncontrolledAccordion/reducer.test.tsx
+++ src/components/UncontrolledAccordion/reducer.test.tsx
@@ -3,7 +3,14 @@ import {reducer} from "./reducer";
 test('collapsed should be true', () => {
 
     const state: boolean = false
-    const newState = reducer(state, {type:'TOGGLE-COLLAPSED'});
+    const newState = reducer(state, {type: 'TOGGLE-COLLAPSED'});
 
     expect(newState).toBe(true)
+})
+
+test('fake should be true', () => {
+    const state: boolean = false
+    const newBooleanTrue = reducer(state, {type: 'FAKE'})
+
+    expect(newBooleanTrue).toBe(true)
 })
--- src/components/UncontrolledAccordion/reducer.tsx
+++ src/components/UncontrolledAccordion/reducer.tsx
@@ -7,6 +7,8 @@ export const reducer = (state: boolean, action: ActionType) => {
     switch (action.type) {
         case 'TOGGLE-COLLAPSED':
             return !state
+        case 'FAKE':
+            return true
         default:
             throw new Error('Bad action type')
     }
